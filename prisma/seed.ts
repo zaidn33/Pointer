62e79802fefe811596154de4208b78883ee9c0b4
Zaid Nausath <z.nausahd11@gmail.com>
2026-04-16 00:58:29 -0400

    Allow prisma seed to load env from .env.local

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,6 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+dotenv.config({ path: ".env.local" });
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
